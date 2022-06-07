b1f7395eb33eedd27cb745a1ee7731da4f506dc8
kwonjs1996 <kwonjs1996@gmail.com>
2022-06-07 17:52:03 +0900

    bmi계산기

---
+++ bmi_calculator.py
@@ -0,0 +1,24 @@
+# Don't change the code below
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# Don't change the code above
+# 80 / 1.75 ** 1.75 =
+#Write your code below this line
+height1 = float(height)
+weight1 = float(weight)
+
+bmi = weight1/(height1*height1)
+
+bmi1 = int(bmi)
+print(type(bmi1))
+
+
+
+# bmi = float(weight)/float(height)**2
+#
+# bmi1 = int(bmi)
+# print(bmi1)
+
+
+
+
