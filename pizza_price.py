70789d696d130c4c081d997e7ae55a7774de54b9
kwonjs1996 <kwonjs1996@gmail.com>
2022-06-12 01:28:25 +0900

    피자 가격 계산기 (답안과 비교)

--- pizza_price.py
+++ pizza_price.py
@@ -5,29 +5,84 @@ add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+# Write your code below this line 👇
 price_s = 15
 price_m = 20
 price_l = 25
 
 if size == "S":
-  price_s
-  if add_pepperoni == "Y":
-    price_s+=2
-    print(f"Your final bill is: ${price_s}.")
-    if extra_cheese == "Y":
-      price_s += 1
-      print(f"Your final bill is: ${price_s}.")
-    else:
-      price_s
-      print(f"Your final bill is: ${price_s}.")
-  else:
     price_s
-    print(f"Your final bill is: ${price_s}.")
+    if add_pepperoni == "Y":
+        price_s += 2
+        if extra_cheese == "Y":
+            price_s += 1
+            print(f"Your final bill is: ${price_s}.")
+        elif extra_cheese == "N":
+            price_s
+            print(f"Your final bill is: ${price_s}.")
+    elif add_pepperoni == "N":
+        price_s
+        if extra_cheese == "Y":
+            price_s += 1
+            print(f"Your final bill is: ${price_s}.")
+        elif extra_cheese == "N":
+            price_s
+            print(f"Your final bill is: ${price_s}.")
+    else:
+        print("You choose wrong, Please check your choice")
 
-
-
-
+elif size == "M":
+    price_m
+    if add_pepperoni == "Y":
+        price_m += 3
+        if extra_cheese == "Y":
+            price_m += 1
+            print(f"Your final bill is: ${price_m}.")
+        elif extra_cheese == "N":
+            price_m
+            print(f"Your final bill is: ${price_m}.")
+    elif add_pepperoni == "N":
+        price_m
+        if extra_cheese == "Y":
+            price_m += 1
+            print(f"Your final bill is: ${price_m}.")
+        elif extra_cheese == "N":
+            price_m
+            print(f"Your final bill is: ${price_m}.")
+    else:
+        print("You choose wrong, Please check your choice")
+elif size == "L":
+    price_l
+    if add_pepperoni == "Y":
+        price_l += 3
+        if extra_cheese == "Y":
+            price_l += 1
+            print(f"Your final bill is: ${price_l}.")
+        elif extra_cheese == "N":
+            price_l
+            print(f"Your final bill is: ${price_l}.")
+    elif add_pepperoni == "N":
+        price_l
+        if extra_cheese == "Y":
+            price_l += 1
+            print(f"Your final bill is: ${price_l}.")
+        elif extra_cheese == "N":
+            price_l
+            print(f"Your final bill is: ${price_l}.")
+    else:
+        print("You choose wrong, Please check your choice")
+else:
+    print("You choose wrong size, Please check your choice")
+    # if add_pepperoni == "Y":
+    #   price_s+=2
+    # elif add_pepperoni == "N":
+    #   price_s
+    #   if extra_cheese == "Y":
+    #     price_s += 1
+    #     print(f"Your final bill is: ${price_s}.")
+    #   if extra_cheese == "N":
+    #     price_s
+    #     print(f"Your final bill is: ${price_s}.")
 
 # else:
 #   print("Small Pizza: $15")
