3479631d2346d94690e391313eeee0fe01481984
kwonjs1996 <kwonjs1996@gmail.com>
2022-06-15 19:23:24 +0900

    for in range 사용

---
+++ even_total.py
@@ -0,0 +1,11 @@
+#Write your code below this row 👇
+total = 0
+for even in range(2, 101, 2):
+  total += even
+print(total)
+
+total1 = 0
+for even1 in range(1,101):
+  if even1 % 2 == 0:
+    total1 += even1
+print(total1)
