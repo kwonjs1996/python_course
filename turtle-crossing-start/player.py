cc0dce71391fd5cdf1bb7b1cb908e2797b654832
kwonjs1996 <kwonjs1996@gmail.com>
2022-07-09 13:42:54 +0900

    level_up 함수 구현

--- turtle-crossing-start/player.py
+++ turtle-crossing-start/player.py
@@ -5,6 +5,7 @@ MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
+
 class Player(Turtle):
     def __init__(self):
         super().__init__()
@@ -14,7 +15,14 @@ class Player(Turtle):
         self.setheading(90)
         self.goto(STARTING_POSITION)
         self.showturtle()
+        self.level_speed = 0.1
+        self.finish_line_y = FINISH_LINE_Y
 
     def move(self):
         new_y = self.ycor() + MOVE_DISTANCE
         self.goto(0, new_y)
+
+    def level_up(self):
+        self.setpos(STARTING_POSITION)
+        self.level_speed *= 0.5
+
