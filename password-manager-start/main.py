42b00b61104eec8f3a3c20a030c211c9b19f88e9
kwonjs1996 <kwonjs1996@gmail.com>
2022-07-15 16:38:05 +0900

    json 활용, 예외처리, 유효성검사

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -2,9 +2,14 @@ import tkinter
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def password_generate():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -19,11 +24,18 @@ def password_generate():
     password_input.insert(0, f"{password}")
     pyperclip.copy(password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add():
     site = site_input.get()
     user_email = email_input.get()
     password = password_input.get()
+    new_data = {
+        site: {
+            "email": user_email,
+            "password": password
+        }
+    }
 
     if site == "" and password == "":
         messagebox.showinfo(title="Website and Password empty", message="Please enter the Website and Password")
@@ -36,14 +48,20 @@ def add():
         is_ok = messagebox.askokcancel(title=site, message=f"These are the details entered: \nEmail: {user_email} "
                                                            f"\nPassword: {password} \nIs it ok to save")
         if is_ok:
-            with open("data.txt", mode="a") as data:
-                data.write(f"{site} | {user_email} | {password}\n")
-            site_input.delete(0, "end")
-            password_input.delete(0, "end")
-            site_input.focus()
-
-
-
+            try:
+                with open("data.json", mode="r") as data_file:
+                    data = json.load(data_file)
+                    data.update(new_data)
+            except FileNotFoundError:
+                with open("data.json", mode="w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                with open("data.json", mode="w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
+                site_input.delete(0, "end")
+                password_input.delete(0, "end")
+                site_input.focus()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -79,11 +97,7 @@ password_input.grid(row=3, column=1)
 generate_button = tkinter.Button(text="Generate Password", command=password_generate)
 generate_button.grid(row=3, column=2)
 
-add_button = tkinter.Button(text="Add",width=36, command=add)
+add_button = tkinter.Button(text="Add", width=36, command=add)
 add_button.grid(row=4, column=1, columnspan=2)
-
-
-
-
 
 window.mainloop()
