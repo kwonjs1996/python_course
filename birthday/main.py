73d9f0d54eba20fb3499f9ca002114666e2623de
kwonjs1996 <kwonjs1996@gmail.com>
2022-10-31 19:01:42 +0900

    birthday gift

---
+++ birthday/main.py
@@ -0,0 +1,20 @@
+print("Hi~~abasdfasdf")
+print("퀴즈 할건데 준비됐나?")
+question1 = input("When is our first anniversary? ex) 3/30\n"
+                  "")
+while not question1 == "2/22":
+    question1 = input("When is our first anniversary? ex) 3/30\n"
+                      "")
+question2 = input("where is the place that we first trip 1/2\n")
+while not question2 == "songdo":
+    question2 = input("where is the place that we first trip 1/2\n")
+question3 = input("When is my birth day?\n")
+while not question3 == "6/23":
+    question3 = input("When is my birth day?\n")
+question4 = input("what date today?\n")
+while not question4 == "11/20":
+    question4 = input("what date today?\n")
+question5 = input("when is your birth day?\n")
+while not question5 == "11/20":
+    question5 = input("when is your birth day?\n")
+print("this is for you")
